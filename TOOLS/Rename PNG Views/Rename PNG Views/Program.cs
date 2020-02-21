41018163b9a5a6eab0e49019e69d5094a3e37699
José Miguel Sánchez Fernández <jose.miguel@visualstudioex3.com>
2020-02-21 15:22:07 +0100

    - DIV2GJAM.PRG: Changed byte type for int in strcuct level_data because not improved memory aligment (in DIV, all types, except string, aligning in 4 bytes, except in arrays). - Added new sprites for mode 7 entities:     - Enemies, boss, asteroids and player-enemy missile.     - FPG for Asteroids 1, 2 and 3. - Added command line tool project, in C#, to ease rename exported views from Unity and renamed with his right id (for ease importing in DIV to add to FPG file). - Updated .gitignore with Visual Studio ignores.

---
+++ TOOLS/Rename PNG Views/Rename PNG Views/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace DIV2GJAM_Rename_Views_BMPs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char index = Path.GetFileName(Directory.GetCurrentDirectory())[0];
+
+            for (int i = 0; i < 5; i++)
+            {
+                Rename($"{i}", $"{index}0{i}");
+            }
+
+            for (int i = 60, j = 14; i < 64; i++, j--)
+            {
+                Rename($"{i}", $"{index}{j}");
+            }
+        }
+
+        static void Rename(string oldFilename, string newFilename)
+        {
+            File.Move($"{oldFilename}.png", $"{newFilename}.png");
+        }
+    }
+}
