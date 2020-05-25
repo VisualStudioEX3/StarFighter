5029b522ff0e422110e3f740f78792190a0b4dc9
José Miguel Sánchez Fernández <jose.miguel@visualstudioex3.com>
2020-05-25 23:21:04 +0200

    DLL projects: - Added new CONFIG.DLL project:     - Added source files.     - Added external INI parser implementation from https://github.com/compuphase/minIni and adapted to use DIV I/O functions.     - Added MAKE.BAT command to pipeline.

---
+++ DLL/CONFIG/minGlue-DIV.h
@@ -0,0 +1,35 @@
+/*  Glue functions for the minIni library, based on the C/C++ stdio library and DIV I/O file functions.
+ *
+ *  Or better said: this file contains macros that maps the function interface
+ *  used by minIni to the standard C/C++ and DIV file I/O functions.
+ *
+ *  By CompuPhase, 2008-2014
+ *  (C) VisualStudioEX3, José Miguel Sánchez Fernández - 2020
+ *  DIV Games Studio 2 (C) Hammer Technologies - 1998, 1999
+ *
+ *  This "glue file" is in the public domain. It is distributed without
+ *  warranties or conditions of any kind, either express or implied.
+ */
+
+/* map required file I/O types and functions to the standard C library and DIV */
+#include <stdio.h>
+#include "..\div.h"
+
+#define INI_FILETYPE                    FILE*
+#define ini_openread(filename,file)     ((*(file) = div_fopen((filename),"rb")) != NULL)
+#define ini_openwrite(filename,file)    ((*(file) = div_fopen((filename),"wb")) != NULL)
+#define ini_openrewrite(filename,file)  ((*(file) = div_fopen((filename),"r+b")) != NULL)
+#define ini_close(file)                 (div_fclose(*(file)) == 0)
+#define ini_read(buffer,size,file)      (fgets((buffer),(size),*(file)) != NULL)
+#define ini_write(buffer,file)          (fputs((buffer),*(file)) >= 0)
+#define ini_rename(source,dest)         (rename((source), (dest)) == 0)
+#define ini_remove(filename)            (remove(filename) == 0)
+
+#define INI_FILEPOS                     long int
+#define ini_tell(file,pos)              (*(pos) = ftell(*(file)))
+#define ini_seek(file,pos)              (fseek(*(file), *(pos), SEEK_SET) == 0)
+
+/* for floating-point support, define additional types and functions */
+#define INI_REAL                        float
+#define ini_ftoa(string,value)          sprintf((string),"%f",(value))
+#define ini_atof(string)                (INI_REAL)strtod((string),NULL)
